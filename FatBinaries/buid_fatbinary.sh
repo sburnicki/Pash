84d03593ecb5ebaddf29dddd5878991a7e4c545f
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-09-16 10:17:40 +0200

    Script to create fat binaries

---
+++ FatBinaries/buid_fatbinary.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+xbuild /p:Configuration=Release
+
+dir="../Source/PashConsole/bin/Release/"
+exe="${dir}/Pash.exe"
+dlls="${dir}/*.dll"
+
+if [ "$1" = "-32bit" ]; then
+	export AS="as -arch i386"
+	export CC="cc -arch i386 -lobjc -liconv -framework Foundation"
+fi
+mkbundle --deps --static -o Pash $exe $dlls
